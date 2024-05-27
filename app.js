b3d084aa640521be623690bb6defb6ea7314585c
Victor <matiasvferrada@hotmail.com>
2024-05-27 16:33:55 -0400

    a

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-let loans = {
-    hipotecario: 0,
-    automotriz: 0,
-    consumo: 0
-  };
-  
-  function guardar() {
-    let loanType = document.getElementById("loanType").value;
-    loans[loanType]++;
-  }
-  
-  function mostrar() {
-    let summary = `Hipotecarios: ${loans.hipotecario}\n
-                   Automotriz: ${loans.automotriz}\n
-                   Consumo: ${loans.consumo}`;
-    
-    alert(summary);
-  }
+let clientes = []
+
+function guardar(formulario){
+    const {Nomcli, tipo} = formulario;
+    if (Nomcli.value.trim() === "") {
+        alert("El nombre del cliente no puede estar en blanco.");
+        return false;
+    }
+    let datosclientes = {nombre: Nomcli.value, tipo: tipo.value};
+    clientes.push(datosclientes);
+    console.log(clientes);
+    formulario.reset();
+    return false;
+}
+
+function mostrar() {
+    let resumen = {
+        hipotecario: 0,
+        automotriz: 0,
+        consumo: 0
+    };
+
+    for (let cliente of clientes) {
+        resumen[cliente.tipo]++;
+    }
+
+    let resumenDiv = document.getElementById("resumen");
+    resumenDiv.innerHTML = `Hipotecarios: ${resumen.hipotecario}<br>
+                            Automotriz: ${resumen.automotriz}<br>
+                            Consumo: ${resumen.consumo}`;
+}
+
